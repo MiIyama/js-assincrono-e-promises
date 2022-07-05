8f47c5f312befb074d98205d45863a527d6ec6b8
MiIyama <44329442+MiIyama@users.noreply.github.com>
2022-07-05 10:13:01 -0300

    add axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-fetch("https://api.github.com/users/maykbrito")
-  .then((response) => response.json())
-  .then((data) => fetch(data.repos_url))
-  .then((res) => res.json())
-  .then((d) => console.log(d))
-  .catch((err) => console.log(err));
+import axios from "axios";
+axios.get('https://api.github.com/users/maykbrito')
+  .then(res => {
+    console.log(res.data);
+  });
