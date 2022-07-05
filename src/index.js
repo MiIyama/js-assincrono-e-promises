588087c81e6cc16ebc15da9fed87372525e52962
MiIyama <44329442+MiIyama@users.noreply.github.com>
2022-07-05 10:29:59 -0300

    add axios promisse

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import axios from "axios";
+import axios from'axios'
 
 axios
   .get('https://api.github.com/users/maykbrito')
-  .then(response=>{
-    console.log(response.data)
-  .catch(error=>console.log(error))
-  })
+  .then(response => {
+    const user = response.data
+
+    axios.get(user.repos_url)
+    .then(repos => console.log(repos.data))
+    .catch(error => console.log(error))
+ })
+  .catch(error => console.log(error))
