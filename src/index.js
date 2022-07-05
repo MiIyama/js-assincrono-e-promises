9f961d935323bf350912d40b47a4d9ac060f044a
MiIyama <44329442+MiIyama@users.noreply.github.com>
2022-07-05 10:32:15 -0300

    add axios - promise chaining

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import axios from'axios'
+import axios from "axios";
 
 axios
-  .get('https://api.github.com/users/maykbrito')
-  .then(response => {
-    const user = response.data
-
-    axios.get(user.repos_url)
-    .then(repos => console.log(repos.data))
-    .catch(error => console.log(error))
- })
-  .catch(error => console.log(error))
+  .get("https://api.github.com/users/maykbrito")
+  .then((response) => {
+    const user = response.data;
+    return axios.get(user.repos_url);
+  })
+  .then((repos) => console.log(repos.data))
+  .catch((error) => console.log(error));
